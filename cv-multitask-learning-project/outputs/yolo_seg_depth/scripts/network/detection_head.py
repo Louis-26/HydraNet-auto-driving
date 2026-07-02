d4f772507d30c73ba9d35fb8e64c7d5a4ceff56e
Yi Lu <121090386@link.cuhk.edu.cn>
2026-07-02 13:15:24 -1000

    update

--- cv-multitask-learning-project/outputs/yolo_seg_depth/scripts/network/detection_head.py
+++ cv-multitask-learning-project/outputs/yolo_seg_depth/scripts/network/detection_head.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:532529d5d586c952f8687ebe4421ea64cd11dce8b371c1fb3463fa8dc0920308
-size 7915
+oid sha256:36fe3371516fd121b5b34fa5039edc82ee84aee5462970482011c340ab64013f
+size 7726
